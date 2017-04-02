a720c185dae22471cd1bd3e32906739eff7be1b0
NickSkye <nickskye7@gmail.com>
2017-04-02 11:31:15 -0700

    Initial Commit

---
+++ look/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  look
+//
+//  Created by Nick Hoyt on 4/2/17.
+//  Copyright © 2017 IntelliSkye. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
