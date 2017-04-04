d0f5702759837e8436ea80e6e6e8d0556b28a1b1
NickSkye <nickskye7@gmail.com>
2017-04-04 15:07:17 -0700

    Added a bunch of files that need to be worked on.

---
+++ look/ExploreScreen.swift
@@ -0,0 +1,9 @@
+//
+//  ExploreScreen.swift
+//  look
+//
+//  Created by Nick Hoyt on 4/4/17.
+//  Copyright © 2017 IntelliSkye. All rights reserved.
+//
+
+import Foundation
---
+++ look/PhotoViewController.swift
@@ -0,0 +1,115 @@
+/*Copyright (c) 2016, Andrew Walz.
+
+Redistribution and use in source and binary forms, with or without modification,are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the
+documentation and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS
+BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. */
+
+import UIKit
+import SwiftyCam
+
+class PhotoViewController: UIViewController {
+    
+    var username = ""
+    var responseString = ""
+    var allowed = false
+
+	override var prefersStatusBarHidden: Bool {
+		return true
+	}
+
+	private var backgroundImage: UIImage
+
+	init(image: UIImage) {
+		self.backgroundImage = image
+		super.init(nibName: nil, bundle: nil)
+	}
+
+	required init?(coder aDecoder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		self.view.backgroundColor = UIColor.gray
+		let backgroundImageView = UIImageView(frame: view.frame)
+		backgroundImageView.contentMode = UIViewContentMode.scaleAspectFit
+		backgroundImageView.image = backgroundImage
+		view.addSubview(backgroundImageView)
+		let cancelButton = UIButton(frame: CGRect(x: 10.0, y: 10.0, width: 30.0, height: 30.0))
+		cancelButton.setImage(#imageLiteral(resourceName: "cancel"), for: UIControlState())
+		cancelButton.addTarget(self, action: #selector(cancel), for: .touchUpInside)
+		view.addSubview(cancelButton)
+        
+         let test = CGFloat((view.frame.width - (view.frame.width / 2 + 37.5)) + ((view.frame.width / 2) - 37.5) - 9.0)
+        
+        let sendButton = UIButton(frame: CGRect(x: test, y: view.frame.height - 77.5, width: 18.0, height: 30.0))
+        sendButton.setImage(#imageLiteral(resourceName: "flashOutline"), for: UIControlState())
+        sendButton.addTarget(self, action: #selector(submitToDB), for: .touchUpInside)
+        self.view.addSubview(sendButton)
+	}
+
+	func cancel() {
+		dismiss(animated: true, completion: nil)
+	}
+    
+    func submitToDB() {
+        
+        postImageToServer(image: "test.png", latitude: "69", longitude: "69", username: "test")
+    }
+    
+    ///
+    func postImageToServer(image: String, latitude: String, longitude: String, username: String){
+        //Contingency Handling. Error handling etc.
+        
+        var request = URLRequest(url: URL(string: "http://www.Intelliskye.com/LookPhp/SetLeaderboard.php")!)
+        
+        request.httpMethod = "POST"
+        
+        var postString: String!
+        print("username to post \(username)")
+        postString = "username=\(username)&image=\(image)&latitude=\(latitude)&longitude=\(longitude)"
+        request.httpBody = postString.data(using: String.Encoding.utf8)
+        
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data, error == nil else {                                                 // check for fundamental networking error
+                print("error=\(error)")
+                return
+            }
+            
+            if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 200 {           // check for http errors
+                print("statusCode should be 200, but is \(httpStatus.statusCode)")
+                print("response = \(response)")
+            }
+            
+            self.responseString = String(data: data, encoding: .utf8)!
+            print("responseString = \(self.responseString)")
+            
+        }
+        
+        checkResponseString()
+        task.resume()
+    }
+    
+    
+    func checkResponseString(){
+        print(allowed)
+        if(self.responseString == "Success"){
+            allowed = true
+            print("true///////////////////////////////")
+            
+        }
+        else{
+            allowed = false
+            print("FALSE//////////////////")
+        }
+    }
+}
