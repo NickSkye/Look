39c9f91cc21e92e537d0484ac5075bac1d285617
NickSkye <nickskye7@gmail.com>
2017-04-06 17:18:49 -0700

    profile screen closer and picture screen now works. still need to clean up ui and implement other screens and s3 bucket as well as fix php and connect to database for login and register and post pictures

--- look/PictureViewScreen.swift
+++ look/PictureViewScreen.swift
@@ -7,3 +7,36 @@
 //
 
 import Foundation
+
+import UIKit
+import AVFoundation
+import AVKit
+import SwiftyCam
+
+class PictureViewScreen: UIViewController {
+    
+    
+    @IBOutlet var pictureImage: UIImageView!
+    
+    var selectedPic = ""
+    var username = ""
+    var selectedPicLat = ""
+    var selectedPicLng = ""
+    var selectedPicDate = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let imgURL = NSURL(string: selectedPic)
+        
+        if imgURL != nil{
+            let data = NSData(contentsOf: (imgURL as? URL)!)
+            pictureImage!.image = UIImage(data: data as! Data)
+        }
+      //use selected pic url pictureImage.image = UIImage(data: <#T##Data#>)
+        
+        
+    }
+    @IBAction func backButton(_ sender: Any) {
+        self.performSegue(withIdentifier: "pictureToProfile", sender: self)
+    }
+}
--- look/ProfileScreen.swift
+++ look/ProfileScreen.swift
@@ -15,12 +15,17 @@ import AVFoundation
 import AVKit
 import SwiftyCam
 
-class ProfileScreen: UIViewController {
+class ProfileScreen: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     var username = ""
     var pictureArray = [String]()
-    var latLngArray = [String]()
+    var latArray = [String]()
+    var lngArray = [String]()
     var dateArray = [String]()
+    var selectedPic = ""
+    var selectedPicLat = ""
+    var selectedPicLng = ""
+    var selectedPicDate = ""
     @IBOutlet var profilePicture: UIImageView!
     @IBOutlet var usernameLbl: UILabel!
     @IBOutlet var currentLatLng: UILabel!
@@ -28,22 +33,46 @@ class ProfileScreen: UIViewController {
     @IBOutlet var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //TEST
         profilePicture.image = #imageLiteral(resourceName: "testProfPic")
         usernameLbl.text = "NickSkye"
         currentLatLng.text = "69.69, 69.69"
-        
-        
+        pictureArray.append("https://images-na.ssl-images-amazon.com/images/M/MV5BMTk0ODI2NTEzMl5BMl5BanBnXkFtZTcwNDY5MjI3NA@@._V1_UY317_CR17,0,214,317_AL_.jpg")
+        latArray.append("69.69")
+        lngArray.append("69.69")
+        dateArray.append("69/69/69")
+        ///////////
+        tableView.reloadData()
     }
+    
+    
+    @IBAction func backButton(_ sender: Any) {
+        self.performSegue(withIdentifier: "profileToMain", sender: self)
+    }
+    
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        print(pictureArray.count)
         return pictureArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell") as! ProfileCell
-        cell.imageV.image = UIImage(named: "testProfPic")
-        cell.latlngLbl.text = String(latLngArray[indexPath.row])
+        
+        let imgURL = NSURL(string: pictureArray[indexPath.row])
+        
+        if imgURL != nil{
+            let data = NSData(contentsOf: (imgURL as? URL)!)
+            cell.imageV!.image = UIImage(data: data as! Data)
+        }
+       
+        selectedPic = pictureArray[indexPath.row]
+        cell.latlngLbl.text = String(latArray[indexPath.row])
+        selectedPicLat = latArray[indexPath.row]
+        selectedPicLng = lngArray[indexPath.row]
         cell.dateLbl.text = String(dateArray[indexPath.row])
+        selectedPicDate = dateArray[indexPath.row]
         
         
         // cell.viewButton.tag = indexPath.row
@@ -53,7 +82,7 @@ class ProfileScreen: UIViewController {
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        self.performSegue(withIdentifier: "contestscreenSegue", sender: self)
+        self.performSegue(withIdentifier: "profileToPicture", sender: self)
     }
     
 
@@ -63,6 +92,14 @@ class ProfileScreen: UIViewController {
         if segue.identifier == "mainToCamera" {
             let DestViewController : ViewController = segue.destination as! ViewController
             DestViewController.username = username
+        }
+        if segue.identifier == "profileToPicture" {
+            let DestViewController : PictureViewScreen = segue.destination as! PictureViewScreen
+            DestViewController.username = username
+            DestViewController.selectedPic = selectedPic
+            DestViewController.selectedPicLat = selectedPicLat
+            DestViewController.selectedPicLng = selectedPicLng
+            DestViewController.selectedPicDate = selectedPicDate
         }
         
         
